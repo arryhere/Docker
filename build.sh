05b17cab64e3b196a37e5e82894be40c3d220c50
Arijit <arijitdasofficial@gmail.com>
2024-01-15 02:01:05 +0530

    feat: docker image build push

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "Docker: start"
 
-docker image build -t nodeapp:1.0.0 ./
+docker image build -t nodeapp:1.0.0 -f ./Dockerfile ./
 docker container run -d -p 3001:3000 --name nodeapp nodeapp:1.0.0
 docker tag nodeapp:1.0.0 arryhere/nodeapp:v1.0.0
 docker push arryhere/nodeapp:v1.0.0
