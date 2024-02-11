64cfe875be8d13b75f6ba6c9aee4dd0985d886aa
Arijit <arijitdasofficial@gmail.com>
2024-02-12 03:29:16 +0530

    feat: server_2

--- docker-reset.sh
+++ docker-reset.sh
@@ -5,11 +5,12 @@ echo "Docker: reset start"
 docker container stop $(docker container ls -a -q)
 docker container rm $(docker container ls -a -q)
 docker image rm $(docker container ls -a -q)
+docker volume rm $(docker volume ls -q)
 docker system prune -a -f
 
 echo "Docker: reset end"
 
 sleep 2
 
-echo "CLearing console"
+echo "Clearing console"
 clear
