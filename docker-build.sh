c28ff5c6b7a6b12b1e75e05fa6ab0c93c42c7808
Arijit Das [Itobuz] <arijit@itobuz.com>
2024-12-30 16:04:33 +0530

    feat: docker image build sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 
-if [ "$1" == "server_1" ]
+echo "node_docker: Docker image build start"
 
-  then
-    echo "Dockerfile.server_1: build start"
+docker image build -t node_docker:1.0.0 -f ./docker/Dockerfile ./
+docker tag node_docker:1.0.0 arryhere/node_docker:1.0.0
+docker push arryhere/node_docker:1.0.0
 
-    docker image build -t node-app:1.0.0 -f ./docker/Dockerfile.server_1 ./
-    docker container run -d -p 3001:3000 --name node-app node-app:1.0.0
-    docker tag node-app:1.0.0 arryhere/node-app:v1.0.0
-    docker push arryhere/node-app:v1.0.0
+echo "node_docker: Docker image build and push end"
 
-    echo "Dockerfile.server_1: build end"
-
-elif [ "$2" == "server_2" ]
-
-  then
-    echo "Dockerfile.server_2: build start"
-
-    docker compose -f ./compose.yaml up -d --build
-
-    echo "Dockerfile.server_2: build end"
-
-else
-  echo "choose a Dockerfile !"
-fi
